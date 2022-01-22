0777cbba34833ef8ed89896e7baceed19d2226a3
Niki NIKOLOV <ngenadie@e2r7p17.clusters.42paris.fr>
2022-01-22 17:55:41 +0100

    lets go mlx

---
+++ Makefile
@@ -0,0 +1,44 @@
+NAME			= so_long_mlx
+EX_NAME			= so_long
+
+CC				= gcc
+CFLAGS			= -Wall -Wextra -Werror
+
+FLAGS			= -L ${LIBFT_D} -lft
+RM				= rm -f
+
+SRC_D			= srcs
+SRCS_LIST		= ft_so_long.c
+SRC_C			= $(addprefix ${SRC_D}/, ${SRCS_LIST})
+
+INC_D			= includes
+HEADER_LIST		= ft_so_long.h
+HEADER_H		= $(addprefix ${INC_D}/, ${HEADER_LIST})
+
+LIBFT			= libft.a
+LIBFT_D			= libft
+LIBFT_A			= $(addprefix ${LIBFT_D}/, ${LIBFT})
+
+OBJS			= ${SRC_C:.c=.o}
+
+all:			${NAME}
+
+$(NAME): $(OBJS)
+	make -C ${LIBFT_D}
+	cp ${LIBFT_D}/${LIBFT} .
+	$(CC) $(OBJS) libft.a libmlx_Linux.a \
+	-Imlx -lXext -lX11 -lm -lz -Lmlx -lmlx -o $(EX_NAME)
+
+%.o: %.c ${HEADER_H}
+	$(CC) $(CFLAGS) libmlx_Linux.a \
+	-I/usr/bin -I${INC_D} -I${LIBFT_D} -Imlx -c $< -o $@
+
+clean:
+				$(MAKE) -C ${LIBFT_D} clean
+				${RM} ${OBJS}
+
+fclean: clean
+				$(MAKE) -C ${LIBFT_D} fclean
+				${RM} ${EX_NAME}
+
+re: 			fclean all
---
+++ srcs/ft_so_long.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_so_long.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngenadie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/06 22:10:56 by ngenadie          #+#    #+#             */
+/*   Updated: 2022/01/22 15:48:03 by ngenadie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_so_long.h"
+
+int main(int argc, char **argv)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	int		endian;
+	int		bits_per_pixel;
+	int		size_line;
+
+	(void)argc;
+	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
+		printf("failed to initialize mlx");
+
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, argv[1]);
+	if (win_ptr == NULL)
+		printf("failed to initialize window");
+
+	img_ptr = mlx_new_image(mlx_ptr, 3000, 3000);
+	if (img_ptr == NULL)
+		printf("failed to create image");
+
+	mlx_get_data_addr(img_ptr, &bits_per_pixel, &size_line, &endian);
+	printf("bits_per_pixel: %d, size_line: %d, endian: %d\n", bits_per_pixel, size_line, endian);
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, mlx_get_color_value(mlx_ptr, 1638400));
+	mlx_loop(mlx_ptr);
+}
