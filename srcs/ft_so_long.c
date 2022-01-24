e9ad192508eed38f0ecf6bf543d4999508debd52
Niki NIKOLOV <ngenadie@e2r11p10.clusters.42paris.fr>
2022-01-24 20:41:30 +0100

    xpm file to image fails

--- srcs/ft_so_long.c
+++ srcs/ft_so_long.c
@@ -6,7 +6,7 @@
 /*   By: ngenadie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 22:10:56 by ngenadie          #+#    #+#             */
-/*   Updated: 2022/01/24 00:32:25 by ngenadie         ###   ########.fr       */
+/*   Updated: 2022/01/24 20:35:04 by ngenadie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int ft_draw_square(t_mlx_data data, char *path_string, int i, int j)
 {
-	printf("DRAWING2");
+	printf("data_width = %i\ndata_height = %i\n",
+			data.width, data.height);
 	data.img_ptr = mlx_xpm_file_to_image(data.mlx_ptr, path_string,
 										&data.width, &data.height);
 	if (data.img_ptr == NULL)
@@ -30,7 +31,6 @@ int ft_draw_board(t_mlx_data data, t_dynarray darr)
 	uint64_t	j;
 	char		**board;
 
-	printf("DRAWING");
 	i = 0;
 	j = 0;
 	board = ((char**)darr.list);
@@ -38,6 +38,7 @@ int ft_draw_board(t_mlx_data data, t_dynarray darr)
 	{
 		while (board[i][j])
 		{
+			printf("board[i][j] = %c\n", board[i][j]);
 			if (board[i][j] == 'P')
 				if (ft_draw_square(data, "./images/hero", i, j) == -1)
 					return (-1);
@@ -58,14 +59,25 @@ int ft_draw_board(t_mlx_data data, t_dynarray darr)
 	return (0);
 }
 
-int ft_get_map(char **argv, t_dynarray darr)
+void ft_print_darr(t_dynarray darr)
+{
+	uint64_t i;
+
+	i = 0;
+	dprintf(1, "nb_cells = %lu\n", darr.nb_cells);
+	while (i < darr.nb_cells)
+	{
+		printf("line[%lu] = %s\n", i, ((char**)darr.list)[i]);
+		i++;
+	}
+}
+
+int ft_get_map(char **argv, t_dynarray *darr)
 {
 	int		fd;
 	int		ret;
 	char	*s;
-	char		**board;
 
-	board = ((char**)darr.list);
 	s = NULL;
 	if ((fd = open(argv[2], O_RDONLY)) == -1)
 		return (-1);
@@ -76,11 +88,9 @@ int ft_get_map(char **argv, t_dynarray darr)
 			printf("ret: -1");
 			return (-1);
 		}
-		if (push_dynarray(&darr, &s, 1, 1) == -1)
+		if (push_dynarray(darr, &s, 1, 1) == -1)
 			return (-1);
 	}
-	dprintf(1, "BEFORE DPRINTF\n");
-	dprintf(1, "board[0][0] = %c\n", board[0][0]);
 	return (0);
 }
 
@@ -88,12 +98,12 @@ int main(int argc, char **argv)
 {
 	t_mlx_data	data;
 	t_dynarray	darr;
-	int			endian;
-	int			bits_per_pixel;
-	int			size_line;
 
 	(void)argc;
 	init_dynarray(&darr, 0, 8);
+	data.width = 48;
+	data.height = 48;
+
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		printf("failed to initialize mlx");
@@ -106,15 +116,15 @@ int main(int argc, char **argv)
 	if (data.img_ptr == NULL)
 		printf("failed to create image");
 
-	mlx_get_data_addr(data.img_ptr, &bits_per_pixel, &size_line, &endian);
-	printf("bits_per_pixel: %d, size_line: %d, endian: %d\n",
-			bits_per_pixel, size_line, endian);
-
-	if (ft_get_map(argv, darr) == -1)
+	data.img_ptr = mlx_xpm_file_to_image(data.mlx_ptr, "./images/rock",
+						&data.width, &data.height);
+	printf("data.img_ptr = %p\n", data.img_ptr);
+	if (ft_get_map(argv, &darr) == -1)
 	{
 		printf("get_map: -1");
 		return (-1);
 	}
+	ft_print_darr(darr);
 	if (ft_draw_board(data, darr) == -1)
 	{
 		printf("draw_board: -1");
